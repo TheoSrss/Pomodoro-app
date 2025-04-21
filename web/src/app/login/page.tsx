2baa5c0ceadd78b942323000ab1c8e29ecca3fe7
Theo <73302483+TheoSrss@users.noreply.github.com>
2025-04-21 18:55:37 +0200

    front | fix for deploy

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,20 +1,20 @@
-"use client";
+"use client"
 
-import { Button } from "@/components/ui/button";
-import { Error } from "@/components/ui/error";
-import { Input } from "@/components/ui/input";
-import { signIn } from "next-auth/react";
-import Image from "next/image";
-import { useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation"
+import { useEffect, useState, Suspense } from "react"
+import { signIn } from "next-auth/react"
+import { Button } from "@/components/ui/button"
+import { Error } from "@/components/ui/error"
+import { Input } from "@/components/ui/input"
+import Image from "next/image"
 
-export default function Login() {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+function LoginFormContent() {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [formError, setFormError] = useState<string | null>(null)
 
     const searchParams = useSearchParams()
     const errorSearch = searchParams.get("error")
-    const [formError, setFormError] = useState<string | null>(null)
 
     useEffect(() => {
         if (errorSearch === "OAuthAccountNotLinked") {
@@ -29,27 +29,19 @@ export default function Login() {
     }, [errorSearch])
 
     const handleLogin = async (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
-
+        e.preventDefault()
         await signIn("credentials", {
             email,
             password,
             redirect: true,
             callbackUrl: "/",
-        });
-    };
+        })
+    }
 
     return (
-        <div className="m-auto my-30 min-w-100 max-w-140 flex flex-col py-10 px-20 bg-beige rounded-2xl shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
-            <div className=" w-full flex justify-evenly py-2 mb-7">
-                <span className="font-bold">Login</span>
-                <span>Sign up</span>
-            </div>
-            <h1 className="uppercase text-6xl text-center">Pomodoro</h1>
+        <>
             <form action="#" className="my-5">
-                <Error
-                    message={formError || ''}
-                />
+                <Error message={formError || ""} />
                 <Input
                     type="text"
                     id="email"
@@ -63,15 +55,13 @@ export default function Login() {
                     id="password"
                     placeholder="mypassword"
                     label="Password"
-                    value={password} onChange={(e) => setPassword(e.target.value)}
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
                 />
-                <Button
-                    onClick={handleLogin}
-                    className="mt-5"
-                >
+                <Button onClick={handleLogin} className="mt-5">
                     Log In
                 </Button>
-            </form >
+            </form>
 
             <div className="flex items-center gap-2 my-6">
                 <div className="flex-1 border-t border-gray-300 dark:border-zinc-700 shadow-sm blur-[1px] opacity-70" />
@@ -79,15 +69,30 @@ export default function Login() {
                 <div className="flex-1 border-t border-gray-300 dark:border-zinc-700 shadow-sm blur-[1px] opacity-70" />
             </div>
 
-
             <Button onClick={() => signIn("google", { callbackUrl: "/" })} className="mb-4">
                 <Image src="/google.webp" alt="Google" width={20} height={20} />
                 Google
             </Button>
-            <Button onClick={() => signIn("google", { callbackUrl: "/" })} className="">
-                <Image src="/github.png" alt="Google" width={20} height={20} />
+            <Button onClick={() => signIn("github", { callbackUrl: "/" })}>
+                <Image src="/github.png" alt="Github" width={20} height={20} />
                 Github
             </Button>
-        </div >
-    );
+        </>
+    )
+}
+
+export default function LoginPage() {
+    return (
+        <div className="m-auto my-30 min-w-100 max-w-140 flex flex-col py-10 px-20 bg-beige rounded-2xl shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
+            <div className="w-full flex justify-evenly py-2 mb-7">
+                <span className="font-bold">Login</span>
+                <span>Sign up</span>
+            </div>
+            <h1 className="uppercase text-6xl text-center">Pomodoro</h1>
+
+            <Suspense fallback={<div>Chargement...</div>}>
+                <LoginFormContent />
+            </Suspense>
+        </div>
+    )
 }
