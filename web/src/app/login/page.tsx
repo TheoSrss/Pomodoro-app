6c125f339bea7457b1dc1eb45722bff30618a4d7
Theo <73302483+TheoSrss@users.noreply.github.com>
2025-05-05 17:00:12 +0200

    add register back front

--- web/src/app/login/page.tsx
+++ web/src/app/login/page.tsx
@@ -1,12 +1,12 @@
 "use client"
 
 import { useSearchParams } from "next/navigation"
-import { useEffect, useState, Suspense } from "react"
-import { signIn } from "next-auth/react"
+import { useEffect, useState } from "react"
 import { Button } from "@/components/ui/button"
 import { Error } from "@/components/ui/error"
 import { Input } from "@/components/ui/input"
-import { FcGoogle } from "react-icons/fc";
+import MainAuth from "@/components/authentification/auth"
+import { signIn } from "next-auth/react"
 
 function LoginFormContent() {
     const [email, setEmail] = useState("")
@@ -65,36 +65,14 @@ function LoginFormContent() {
                 </Button>
             </form>
 
-            <div className="flex items-center gap-2 my-6">
-                <div className="flex-1 border-t border-gray-300 dark:border-zinc-700 shadow-sm blur-[1px] opacity-70" />
-                <span className="text-sm text-gray-500 dark:text-gray-400">Continue with</span>
-                <div className="flex-1 border-t border-gray-300 dark:border-zinc-700 shadow-sm blur-[1px] opacity-70" />
-            </div>
-
-            <Button onClick={() => signIn("google", { callbackUrl: "/" })} className="mb-4">
-                <FcGoogle style={{ width: 25, height: 25 }} />
-                Google
-            </Button>
-            {/* <Button onClick={() => signIn("github", { callbackUrl: "/" })}>
-                <Image src="/github.png" alt="Github" width={25} height={25} />
-                Github
-            </Button> */}
         </>
     )
 }
 
 export default function LoginPage() {
     return (
-        <div className="m-4 md:mx-auto my-30 min-w-100 max-w-140 flex flex-col py-10 px-10 md:px-20 bg-beige rounded-2xl shadow-[0.625rem_0.625rem_0.875rem_0_rgb(225,226,228),-0.5rem_-0.5rem_1.125rem_0_rgb(255,255,255)]">
-            <div className="w-full flex justify-evenly py-2 mb-7">
-                <span className="font-bold">Login</span>
-                <span>Sign up</span>
-            </div>
-            <h1 className="uppercase text-6xl text-center">Pomodoro</h1>
-
-            <Suspense fallback={<div>Chargement...</div>}>
-                <LoginFormContent />
-            </Suspense>
-        </div>
+        <MainAuth>
+            <LoginFormContent />
+        </MainAuth>
     )
 }
---
+++ web/src/app/register/page.tsx
@@ -0,0 +1,122 @@
+"use client";
+
+import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
+import MainAuth from "@/components/authentification/auth";
+import { Error } from "@/components/ui/error";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { signIn } from "next-auth/react";
+
+interface ApiViolation {
+    propertyPath: string;
+    message: string;
+}
+
+export default function RegisterPage() {
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [confirmPassword, setConfirmPassword] = useState("");
+    const [formError, setFormError] = useState<string | null>(null);
+    const searchParams = useSearchParams();
+
+    const errorSearch = searchParams.get("error");
+
+    useEffect(() => {
+        if (errorSearch === "CredentialsSignin") {
+            setFormError("Email ou mot de passe incorrect.");
+        } else if (errorSearch === "expired") {
+            setFormError("Session expirée.");
+        } else if (errorSearch) {
+            setFormError("Une erreur est survenue. Veuillez réessayer.");
+        } else {
+            setFormError(null);
+        }
+    }, [errorSearch]);
+
+    function isPasswordValid(password: string): boolean {
+        const regex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+        return regex.test(password);
+    }
+
+    const handleRegister = async (e: React.MouseEvent<HTMLButtonElement>) => {
+        e.preventDefault();
+        setFormError(null);
+
+        if (!isPasswordValid(password)) {
+            setFormError("Mot de passe : au moins 8 caractères, une majuscule, une minuscule, un chiffre et un caractère spécial.");
+            return;
+        }
+        if (password !== confirmPassword) {
+            setFormError("Les mots de passe ne correspondent pas.");
+            return;
+        }
+
+        try {
+            const res = await fetch(`${process.env.NEXT_PUBLIC_URI_API}/register`, {
+                method: "POST",
+                headers: { "Content-Type": "application/json" },
+                body: JSON.stringify({ email, password }),
+            });
+
+            if (!res.ok) {
+                const data = await res.json();
+
+                if (res.status === 422 && data.violations) {
+                    const violation = data.violations.find((v: ApiViolation) => v.propertyPath === "email");
+                    if (violation) {
+                        setFormError(violation.propertyPath + ' : ' + violation.message);
+                        return;
+                    }
+                }
+
+                return;
+            }
+
+            // router.push("/login?success=1");
+            await signIn("credentials", {
+                email,
+                password,
+                redirect: true,
+                callbackUrl: "/",
+            })
+        } catch {
+            setFormError("Erreur réseau.");
+        }
+    };
+
+    return (
+        <MainAuth>
+            <form className="my-5">
+                <Error message={formError || ""} />
+                <Input
+                    type="text"
+                    id="email"
+                    placeholder="thibault@gmail.com"
+                    value={email}
+                    label="Email"
+                    onChange={(e) => setEmail(e.target.value)}
+                />
+                <Input
+                    type="password"
+                    id="password"
+                    placeholder="Mot de passe"
+                    label="Mot de passe"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                />
+                <Input
+                    type="password"
+                    id="confirm-password"
+                    placeholder="Répéter le mot de passe"
+                    label="Confirmer le mot de passe"
+                    value={confirmPassword}
+                    onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <Button onClick={handleRegister} className="mt-5">
+                    Register
+                </Button>
+            </form>
+        </MainAuth>
+    );
+}
